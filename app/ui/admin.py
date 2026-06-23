b37a1ebbbcca7c955748cca448843318c2cb1e51
Omid <omidshadpour25@gmail.com>
2026-06-23 20:04:35 +0330

    Fix localhost connection issue by changing API_URL to 127.0.0.1

--- app/ui/admin.py
+++ app/ui/admin.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import uuid
 
-API_URL = "http://localhost:8000"
+API_URL = "http://127.0.0.1:8000"
 
 st.set_page_config(
     page_title="Customer Support Admin",
